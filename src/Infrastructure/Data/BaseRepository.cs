eb001ac487e7df7e3b2f27f893367ba39f905010
Francesco Dagostino <francescodagostino1889@gmail.com>
2024-10-09 15:37:59 -0300

    I Update BaseRep and IBaseRep

--- src/Infrastructure/Data/BaseRepository.cs
+++ src/Infrastructure/Data/BaseRepository.cs
@@ -10,9 +10,38 @@ namespace Infrastructure.Data
     public class BaseRepository<T> : IBaseRepository<T> where T : class
     {
         private readonly AppDBContext _dbContext;
-        public BaseRepository(AppDBContext dbContext) 
+        public BaseRepository(AppDBContext dbContext) //Constructor de la clase
         {
             _dbContext = dbContext;
+        }
+
+        public T? GetById(int id)
+        {
+            return _dbContext.Set<T>().Find(id);
+        }
+
+        public List<T> GetAll()
+        {
+            return _dbContext.Set<T>().ToList();    
+        }
+
+        public void Update(T entity)
+        {
+            _dbContext.Set<T>().Update(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public T Add(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            _dbContext.SaveChanges();
+            return entity;
         }
     }
 }
